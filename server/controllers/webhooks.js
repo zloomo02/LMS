2c535a9ebb3b31d06b3663b4c47f5b198344c96c
Melek Zaag <melekzloom@gmail.com>
2025-02-26 16:38:45 +0100

    corrected user creation

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -17,7 +17,7 @@ export const clerkWebhooks = async (req, res) => {
       case "user.created": {
         const userData = {
           _id: data.id,
-          email: data.email_address[0].email_address,
+          email: data.email_addresses[0].email_address,
           name: data.first_name + " " + data.last_name,
           imageUrl: data.image_url,
         };
@@ -28,7 +28,7 @@ export const clerkWebhooks = async (req, res) => {
 
       case "user.updated": {
         const userData = {
-          email: data.email_address[0].email_address,
+          email: data.email_addresses[0].email_address,
           name: data.first_name + " " + data.last_name,
           imageUrl: data.image_url,
         };
