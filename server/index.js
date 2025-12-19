d7bf10afc318ec72b3b92a336749e0394601f81d
Melek Zaag <melekzloom@gmail.com>
2025-12-19 01:54:12 +0100

    Fix Express deployment + CORS for Vercel

--- server/index.js
+++ server/index.js
@@ -55,8 +55,6 @@ app.post(
   stripeWebhooks
 );
 
-// Port
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+export default app;
+
